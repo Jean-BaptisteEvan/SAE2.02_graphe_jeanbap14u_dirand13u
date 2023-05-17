642629b753664aef44b1257797e604dbd3f29f5b
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-05-17 09:04:33 +0200

    redefinition de equals dans Noeud

--- Graphe/src/Noeud.java
+++ Graphe/src/Noeud.java
@@ -4,9 +4,18 @@ public class Noeud {
 
     private String nom;
     private List<Arc> adj;
-    
+
     public Noeud(String name){
         this.nom = name;
         this.adj = new List<Arc>();
+    }
+
+    public boolean equals(Noeud o) {
+        boolean bRetour;
+        if(o.nom.equals(this.nom)){
+            bRetour = true;
+        }else {
+            bRetour = false;
+        }
     }
 }
