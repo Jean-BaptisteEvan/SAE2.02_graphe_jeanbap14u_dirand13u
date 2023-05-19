a9950a2c1fc0bba53b59931de6f8012e6a2bb383
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-05-19 08:27:08 +0200

    Ajout du squelette de la classe GrapheListe

---
+++ Graphe/src/GrapheListe.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class GrapheListe implements Graphe{
+
+    private ArrayList<String> ensNom;
+    private ArrayList<Noeud> ensNoeuds;
+
+    public ArrayList<String> listeNoeuds() {
+        return null;
+    }
+
+    public ArrayList<Arc> suivants(String n) {
+        return null;
+    }
+
+    public void ajouterArc(String depart, String destination, double cout){
+        return null;
+    }
+}
