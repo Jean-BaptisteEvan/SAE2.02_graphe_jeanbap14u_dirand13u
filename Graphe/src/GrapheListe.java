75fea183d5efa5cb34da05f7c0853e8a61fb5fb2
Dirand Felicien <felicien.dirand1@gmail.com>
2023-05-22 10:28:39 +0200

    Grapheliste classe

--- Graphe/src/GrapheListe.java
+++ Graphe/src/GrapheListe.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
 
 public class GrapheListe implements Graphe{
-
+    /*
+    liste des nom des objets noeuds
+     */
     private ArrayList<String> ensNom;
+    /*
+    liste des objets noeuds
+     */
     private ArrayList<Noeud> ensNoeuds;
 
     public GrapheListe(){
-        this.ensNom = ;
-        this.ensNoeuds = ;
+        this.ensNom =  new ArrayList<String>();
+        this.ensNoeuds = new ArrayList<Noeud>();
     }
     public ArrayList<String> listeNoeuds() {
 
         for(int i=0;i < this.ensNoeuds.size();i++){
+            Noeud nod = this.ensNoeuds.get(i);
+            nod.
 
         }
 
