45ba00efd31a7900a6cf95bd9a5f00d1375e1a14
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2023-05-23 14:25:20 +0200

    ajout javadoc dans grapheListe

--- Graphe/src/GrapheListe.java
+++ Graphe/src/GrapheListe.java
@@ -1,20 +1,28 @@
-import java.nio.Buffer;
+import java.io.*;
 import java.util.ArrayList;
 
 public class GrapheListe implements Graphe{
     /*
-    liste des nom des objets noeuds
+    liste des noms des objets Nœuds
      */
     private ArrayList<String> ensNom;
     /*
-    liste des objets noeuds
+    liste des objets nœuds
      */
     private ArrayList<Noeud> ensNoeuds;
 
+    /**
+     *contructeur sans parametre de grapheListe
+     */
     public GrapheListe(){
-        this.ensNom =  new ArrayList<String>();
-        this.ensNoeuds = new ArrayList<Noeud>();
+        this.ensNom =  new ArrayList<>();
+        this.ensNoeuds = new ArrayList<>();
     }
+
+    /**
+     * méthode qui retourne tout les noms des noeuds connus du graphe
+     * @return ArrayList<String> liste des noms de noeuds
+     */
     public ArrayList<String> listeNoeuds() {
         ArrayList<String> str = null;
         for (Noeud nod : this.ensNoeuds) {
@@ -25,14 +33,24 @@ public class GrapheListe implements Graphe{
         return str;
     }
 
+    /**
+     * @param n nom d un noeud
+     * @return liste des arc du noeyd demande
+     */
     public ArrayList<Arc> suivants(String n) {
         int i = 0;
-        while (n==this.ensNoeuds.get(i).getNom()) {
+        while (n.compareTo(this.ensNoeuds.get(i).getNom())==0) {
             i = i + 1;
         }
         return this.ensNoeuds.get(i).getAdj();
     }
 
+    /**
+     * methode qui permet d ajouter un arc à un graphe
+     * @param depart nom du noeud de depart
+     * @param destination nom du noeud d arrivee
+     * @param cout prix du passage par cet arc
+     */
     public void ajouterArc(String depart, String destination, double cout) {
         if (!this.ensNom.contains(depart)) {
             ensNoeuds.add(new Noeud(depart));
@@ -44,9 +62,9 @@ public class GrapheListe implements Graphe{
         }
         int i = 0;
         boolean stop = false;
-        while (stop == false) {
+        while (!stop && i<this.ensNoeuds.size()) {
 
-            if (depart == this.ensNoeuds.get(i).getNom()) {
+            if (depart.compareTo(this.ensNoeuds.get(i).getNom()) == 0) {
 
                 this.ensNoeuds.get(i).ajouterArc(destination, cout);
                 stop = true;
@@ -56,15 +74,18 @@ public class GrapheListe implements Graphe{
         }
     }
 
+    /**
+     * @return String qui contient tout les informations du graphe
+     */
     public String toString(){
         //initalisation des variables
-        boolean bTrouv = false;
+        boolean bTrouv;
 
         int j = 0;
 
         StringBuffer output = new StringBuffer();
-        ArrayList<Arc> listArc = new ArrayList<Arc>();
-        // boucles sur tout les noeuds
+        ArrayList<Arc> listArc;
+        // boucles sur tous les noeuds
         for (int i = 0;i<this.ensNom.size();i++){
 
             output.append( this.ensNom.get(i) + " ->" );
@@ -92,14 +113,19 @@ public class GrapheListe implements Graphe{
         return output.toString();
     }
 
+    /**
+     * methode qui retourne en texte pour Graphviz le graphe sur le quel elle est appele
+     * @return String
+     */
     public String toGraphviz(){
         //initalisation des variables
-        boolean bTrouv = false;
+        boolean bTrouv;
 
         int j = 0;
 
-        StringBuffer output = new StringBuffer();
-        ArrayList<Arc> listArc = new ArrayList<Arc>();
+        StringBuilder output = new StringBuilder();
+        output.append("digraph G {\n");
+        ArrayList<Arc> listArc;
         // boucles sur tout les noeuds
         for (int i = 0;i<this.ensNom.size();i++){
 
@@ -124,6 +150,32 @@ public class GrapheListe implements Graphe{
             }
             j = 0;
         }
+        output.append("}\n");
         return output.toString();
+    }
+
+    /**
+     * methode qui génere un objet graphe a partir d un fichier
+     * @param destFich emplacement d un fichier contenant une liste d'arcs
+     */
+    public GrapheListe(String destFich) {
+        this.ensNom =  new ArrayList<>();
+        this.ensNoeuds = new ArrayList<>();
+       try{
+           BufferedReader bReader = new BufferedReader(new FileReader(destFich));
+           String ligne = bReader.readLine();
+
+           while (ligne != null) {
+               String[] splitLigne = ligne.split("\t");
+
+               this.ajouterArc(splitLigne[0], splitLigne[1], Double.parseDouble(splitLigne[2]));
+               ligne = bReader.readLine();
+
+           }
+       }catch(FileNotFoundException e){
+           System.out.println("IOEXCEPTION");
+       }catch(IOException e){
+        System.out.println("no fichier");
+    }
     }
 }
